72f11f6e0096f0f251a1ac2f1da444dbf1b158f7
averyle <ngangianggg@gmail.com>
2021-04-18 23:08:39 -0700

    Improvised

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ struct Color {
     float red;
     float green;
     float blue;
-
+    float sum = red + green + blue;
     static Color random() {
         static std::mt19937 engine{ std::random_device{}() };
         static std::uniform_real_distribution<float> dist{ 0, 1 };
@@ -40,21 +40,23 @@ int main() {
     }
 
     std::cout << "COLORS: [\n";
-    for (auto i = colors.begin(); i != colors.end(); ++i) {
-        std::cout << *i << "\n";
+    for (int i = 0; i < colors.size(); ++i) {
+        std::cout << colors[i] << "\n";
     }
     std::cout << "]\n";
 
     std::cout << "REVERSED COLORS: [\n";
     std::reverse(colors.begin(), colors.end());
-    for (auto i = colors.begin(); i != colors.end(); ++i) {
-        std::cout << *i << "\n";
+    for (int i = 0; i < colors.size(); ++i) {
+        std::cout << colors[i] << "\n";
     }
     std::cout << "]\n";
 
     // 3. find the color in the list that is darkest and print it out
     // the darkest color is defined as the color where the sum of the red, green, and blue components is the smallest
-    auto darkestColor = colors[0];
+    auto blue = [](const Color& c1, const Color& c2) { return (c1.sum < c2.sum); };
+    auto darkestColor = std::min_element(colors.begin(), colors.end(), blue);
+   /* auto darkestColor = colors[0];
     float min = darkestColor.red + darkestColor.green + darkestColor.blue;
     for (int i = 1; i < colors.size(); i++) {
             if ((colors[i].red + colors[i].green + colors[i].blue) < min) {
@@ -62,15 +64,16 @@ int main() {
                 min = darkestColor.red + darkestColor.green + darkestColor.blue;
             }
     }
-    std::cout << "The darkest color is " << darkestColor << "\n";
+    */
+    std::cout << "The darkest color is " << *darkestColor << "\n";
 
     // 4. order the colors from smallest 'blue' value to largest 'blue' value and print them out
     std::vector<Color> blueSorted;
     auto comp = [](const Color& c1, const Color& c2) { return (c1.blue < c2.blue); };
     std::sort(colors.begin(), colors.end(), comp);
     std::cout << "COLORS SORTED BY BLUE: [ \n";
-    for (auto i = colors.begin(); i != colors.end(); ++i) {
-        std::cout << *i << "\n";
+    for (int i = 0; i < colors.size(); ++i) {
+        std::cout << colors[i] << "\n";
     }
     std::cout << "]\n";
 
